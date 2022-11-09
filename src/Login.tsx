9d2a5edf937bf0834fc2d4892351c63847106e7e
2yunseong <dbsdltjd3701@naver.com>
2022-11-09 21:22:00 +0900

    feat(login): localStorage에서 redirectUrl을 가져오는 로직 수행

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useInput from './useInput';
 import './Login.css';
 
@@ -7,7 +7,8 @@ const Login = () => {
     const email = useInput();
     const passwd = useInput();
     const [errorMessage, setErrorMessage] = useState("");
-    
+    const [redirectUrl, setRedirectUrl] = useState("");
+
     const onSubmit = (e:any) => {
         e.preventDefault();
         if(!email.value || !passwd.value){
@@ -24,17 +25,22 @@ const Login = () => {
             data: {
                 userEmail: email.value,
                 password: passwd.value,
+                redirectUrl: redirectUrl
             }
         }).then((response) => {
             if(response.status !== 200){
                 setErrorMessage("아이디나 비밀번호를 다시 입력해주세요.");
             }
+            
         }).catch((error) => {
             // error.request.status 에 따른 로직
             setErrorMessage(() => "로그인에 실패했습니다. 네트워크 환경을 확인해주세요.");
         });
     }
 
+    useEffect(() => {
+        setRedirectUrl(() => localStorage.getItem('redirectUrl') ?? 'https://econovation.kr');
+    }, []);
     return <form onSubmit={onSubmit} className='login-form'>
         <h1>에코노베이션 로그인</h1>
         <input type="text" value={email.value} onChange={email.onChange} placeholder="아이디(example@econovation.kr)"/>
