57f6859131c43993f6a16d0177a8a260feaedfe1
2yunseong <dbsdltjd3701@naver.com>
2022-11-23 13:53:18 +0900

    feat(Login): 리다이렉트를 위한 userEmail 로컬 스토리지에 저장

--- src/Login.tsx
+++ src/Login.tsx
@@ -32,6 +32,7 @@ const Login = () => {
             if(response.status === 200){
                 localStorage.setItem('accessToken', response.data.accessToken);
                 localStorage.setItem('refreshToken', response.data.refreshToken);
+                localStorage.setItem('userEmail', email.value);
                 window.location.href = redirectUrl;
             } 
             
