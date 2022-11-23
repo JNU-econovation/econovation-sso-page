6c8c75353612f73f654394589d88b752b7bdcea4
2yunseong <dbsdltjd3701@naver.com>
2022-11-23 22:51:32 +0900

    feat(SignUp): 이메일 컴포넌트 분리

---
+++ src/Email.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Email = () => {
+    return  (<div>
+    <input type='text' placeholder='이메일'/>
+    <button>이메일 중복확인</button>
+    </div>);
+}
+
+export default Email;
