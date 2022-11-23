534091e4ca21271b4420f2683f0c3ab058021cda
2yunseong <dbsdltjd3701@naver.com>
2022-11-23 15:44:49 +0900

    feat(SignUp): 회원가입 기본 태그 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Login from './Login';
 import { Routes, Route } from 'react-router-dom';
+
+import Login from './Login';
+import SignUp from './SignUp';
 
 import './App.css';
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
     </Routes>
   );
 }
