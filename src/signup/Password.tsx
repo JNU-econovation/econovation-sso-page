5ee7f2d85d0b26101173b187e8caa7cef55d5418
2yunseong <dbsdltjd3701@naver.com>
2022-12-29 01:25:50 +0900

    feat(signup): password 컴포넌트 분리

---
+++ src/signup/Password.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Password = () => {
+    return (<div>
+        <h3>비밀번호</h3>
+            <label>
+                비밀번호 입력 : <input type='password'/>
+            </label>
+            <label>
+                비밀번호 확인 : <input type='password' />
+            </label>
+    </div>);
+}
+
+export default Password;
