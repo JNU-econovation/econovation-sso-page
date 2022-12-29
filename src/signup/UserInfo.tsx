cbc98e864f318727653aa2901dfc4ba01672763f
2yunseong <dbsdltjd3701@naver.com>
2022-12-29 16:19:34 +0900

    feat(signup): 이름 컴포넌트 마크업

---
+++ src/signup/UserInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserInfo = () => {
+    return (
+        <div className='user-container'>
+            <h3>이름</h3>
+            <input type='text'/>
+        </div>
+    );    
+};
+
+export default UserInfo;
